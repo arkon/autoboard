48cacae3a06d31a560c35988d5ddbbf65f3d7b7e
Alexander <alexanderviar@gmail.com>
2019-10-09 09:52:35 -0400

    remove Buffer from base64Utils.js

--- src/scripts/utils/base64Utils.js
+++ src/scripts/utils/base64Utils.js
@@ -1,12 +1,7 @@
 export function encode(data){
-  let buff = Buffer.from(data);
-  let text = buff.toString('base64');
-  return text;
+  return btoa(unescape(encodeURIComponent(data)));
 }
 
 export function decode(data){
-  let buff = Buffer.from(data, 'base64');
-  let text = buff.toString();
-  console.log(text);
-  return text;
+  return decodeURIComponent(escape(atob(data)));
 }
