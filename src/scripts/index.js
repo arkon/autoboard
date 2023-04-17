036a50d686438389aa4f918d68abe293c36b6cf9
arkon <eugcheung94@gmail.com>
2023-04-16 20:02:24 -0400

    Migrate to React 18 createRoot function

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import { render } from 'react-dom';
+import * as ReactDOMClient from 'react-dom/client';
 import * as OfflinePluginRuntime from '@lcdp/offline-plugin/runtime';
 
 import CRCMaker from './components/CRCMaker';
 
 import '../styles/styles.scss';
 
-render(<CRCMaker />, document.getElementById('main'));
+const root = ReactDOMClient.createRoot(document.getElementById('main'));
+root.render(<CRCMaker />);
 
 // Install the service worker after everything instantiates correctly
 OfflinePluginRuntime.install();
